499a221734326212fbee6d8a748c2b101af4ec24
LynAs <szlynas@gmail.com>
2016-02-23 10:30:30 +0600

    initial commit

---
+++ src/main/java/com/lynas/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.lynas.controller;
+
+import com.lynas.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sazzad on 2/11/16.
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    AppUserService appUserService;
+
+    @RequestMapping(value = "/")
+    public String home(){
+
+        return "home";
+    }
+
+
+
+}
---
+++ src/main/java/com/lynas/model/AppUser.java
@@ -0,0 +1,54 @@
+package com.lynas.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by sazzad on 2/11/16.
+ */
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, unique = true)
+    private String userName;
+    @Column(nullable = false)
+    private String password;
+
+
+    public AppUser() {
+    }
+
+    public AppUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
